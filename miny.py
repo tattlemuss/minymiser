d9ce3082f43148585ad97274f44860584dcaabf7
Steven Tattersall <tattlemuss@github.com>
2022-12-26 15:29:53 +0000

    New, LZ-based approach. Results look promising.

--- miny.py
+++ miny.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-
 import numpy as np
 import matplotlib.pyplot as plt
-
 
 def percent(inp, outp):
 	return inp * 100.0 / (inp + outp)
@@ -35,12 +33,15 @@ def find_quick_match(data, pos, dist):
 			best_value = value
 	return match
 
-def encode(data, search_len, stats):
+def create_tokens(data, search_len, stats):
 	pos = 0
 	data_len = len(data)
 	lit_count = 0
 	match_count = 0
 	match_bytes = 0
+
+	packing = []
+	open_literal = bytearray()
 
 	while pos < data_len:
 		(offset, count) = find_quick_match(data, pos, search_len)
@@ -52,13 +53,112 @@ def encode(data, search_len, stats):
 			match_count += 1
 			stats.offsets.append(offset)
 			stats.counts.append(count)
+			if len(open_literal) != 0:
+				packing.append(("L", open_literal))
+				open_literal = bytearray()
+
+			packing.append(("M", count, offset))
 		else:
 			#print("Literal {}".format(data[pos]))
-			pos += 1
+			open_literal.append(data[pos])
 			lit_count += 1
+			pos += 1
+
+	if len(open_literal) != 0:
+		packing.append(("L", open_literal))
+		open_literal = bytearray()
+
 	#print("Done")
 	print("Matches {} Literals {} ({:.2f})%".format(match_count, lit_count, percent(match_count, lit_count)))
 	print("Match bytes {} of {} {:.1f}%".format(match_bytes, data_len, 100 * match_bytes / data_len))
+	return packing
+
+def unpack(input):
+
+	class Input:
+		def __init__(self, input):
+			self.input = input
+			self.pos = 0
+
+		def byte(self):
+			a = self.input[self.pos]
+			self.pos += 1
+			return a
+
+	i = Input(input)
+	output = bytearray()
+	while i.pos < len(input):
+		cmd = i.byte()
+		if cmd & 0x80:
+			# Literals
+			count = cmd & 0x7f
+			if count == 0:
+				count = i.byte() << 8 | i.byte()
+			for x in range(0, count):
+				output.append(i.byte())
+		else:
+			count = cmd & 0x7f
+			if count == 0:
+				count = i.byte() << 8 | i.byte()
+			offset = i.byte()
+			if offset == 0:
+				offset = i.byte() << 8 | i.byte()
+			for x in range(0, count):
+				v = output[len(output) - offset]
+				output.append(v)
+	return output
+
+def create_packed(packed):
+	""" Format:
+	
+		byte		bit 7: 1 == literals 0 == match
+					bits 6-0: if 0, long count (2 bytes)
+							  if != 0, count-1 (7 bits 0-128)
+		if long_count:
+			2 bytes - count 0-fffff
+
+		--------
+		if literals: stop
+		--------
+		if matches:
+		byte		if 0, long_count
+
+		if long_count:
+			2 bytes - count 0-fffff
+	"""
+
+	def output_count(count, literal_flag):
+		assert(count > 0)
+		if count < 128:
+			output.append(count | literal_flag)
+		else:
+			output.append(0 | literal_flag)
+			output.append(count >> 8)
+			output.append(count & 255)
+
+	def output_offset(offset):
+		assert(offset > 0)
+		if offset < 256:
+			output.append(offset)
+		else:
+			output.append(0)
+			output.append(offset >> 8)
+			output.append(offset & 255)
+
+	output = bytearray()
+	for p in packed:
+		#print(p)
+		if p[0] == "L":
+			# literals
+			lits = p[1]
+			count = len(lits)
+			output_count(count, 0x80)
+			output += lits
+		else:
+			count, offset = p[1:]
+			output_count(count, 0)
+			output_offset(offset)
+	return output
 
 def read_ym(strm, outstrm):
 	head = strm.read(4)
@@ -74,21 +174,32 @@ def read_ym(strm, outstrm):
 	num_vbls = int(len(all_data) / 14)
 	stats = Stats()
 
+	total = 0
 	for r in range(0, 14):
 		base = r * num_vbls
 		reg_0 = all_data[base:base+num_vbls]
 		print("==== reg {} ====".format(r))
-		encode(reg_0, 8192, stats)
+		tokens = create_tokens(reg_0, 512, stats)
 
-	plt.scatter(stats.offsets, stats.counts, s=1, alpha=0.5)
-	#print(offsets)
-	#plt.hist(offsets, bins=128)
+		b = create_packed(tokens)
+		print(len(b))
+		u = unpack(b)
+		print(len(u))
+
+		total += len(b)
+		assert(bytes(u) == reg_0)
+
+	print("Total size ", total)
+	#plt.scatter(stats.offsets, stats.counts, s=1, alpha=0.5)
+	#plt.show()
+	#plt.hist(stats.offsets, bins=128)
+	#plt.show()
 	#plt.hist(stats.counts, bins=128)
+	#plt.show()
 
+	#for l in pack[0]:
+	#	print(l)
 
-	plt.show()
-
-
-read_ym(open("led1.ym", "rb"), open("led1.ymp", "wb"))
+#read_ym(open("led1.ym", "rb"), open("led1.ymp", "wb"))	 WRONG FORMAT
 #read_ym(open("sanxion.ym", "rb"), open("sanxion.ymp", "wb"))
-#read_ym(open("motus.ym", "rb"), open("motus.ymp", "wb"))
+read_ym(open("motus.ym", "rb"), open("motus.ymp", "wb"))

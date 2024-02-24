f591b05f201c4e53aa9b35131adae21b430d1ef8
Steven Tattersall <2294514+tattlemuss@users.noreply.github.com>
2024-02-24 10:41:48 +0000

    Formatting was a mess.

--- player.s
+++ player.s
@@ -3,21 +3,21 @@
 main:
 	; Allocate memory
 	movea.l	a7,a5
-	movea.l	4(a5),a5		;base page
-	move.l	12(a5),d0		;text size
-	add.l	20(a5),d0		;data size
-	add.l	28(a5),d0		;bss size
-	addi.l	#$0500,d0		;stack size
-	move.l	d0,d1			;copy total size
-	add.l	a5,d1			;plus base size
-	and.l	#-2,d1			;align
-	movea.l	d1,a7			;new stack address
-	move.l	d0,-(a7)		;size
-	move.l	a5,-(a7)		;base of our prog
-	clr.w	-(a7)			;dummy parameter
-	move.w	#$4a,-(a7)		;mshrink()
+	movea.l	4(a5),a5			;base page
+	move.l	12(a5),d0			;text size
+	add.l	20(a5),d0			;data size
+	add.l	28(a5),d0			;bss size
+	addi.l	#$0500,d0			;stack size
+	move.l	d0,d1				;copy total size
+	add.l	a5,d1				;plus base size
+	and.l	#-2,d1				;align
+	movea.l	d1,a7				;new stack address
+	move.l	d0,-(a7)			;size
+	move.l	a5,-(a7)			;base of our prog
+	clr.w	-(a7)				;dummy parameter
+	move.w	#$4a,-(a7)			;mshrink()
 	trap	#1				;call gemdos
-	lea	12(a7),a7		;fix stack
+	lea	12(a7),a7			;fix stack
 
 	; Run core
 	pea	modplay_loop
@@ -35,7 +35,7 @@ main:
 	; Clear player system
 	; Exit
 
-	clr.l	-(sp)			;pterm
+	clr.w	-(sp)				;pterm
 	trap	#1				;call gemdos
 
 modplay_loop:
@@ -52,14 +52,8 @@ modplay_loop:
 	move.l	$114.w,old_c
 	move.l	#c_routine,$114.w
 	move.b	#%1000000,$17(a0)		; irq vector register (automat. eoi)
-
 	clr.b	$484.w
 	move.w	#$2300,sr			; interrupts on
-
-;	move.w	#1000,d7
-;.unp_loop:
-;	bsr	player_update
-;	dbf	d7,.unp_loop
 
 .key:	
 	cmp.b 	#$39,$fffffc02.w
@@ -73,34 +67,36 @@ modplay_loop:
 c_routine
 	move.w	#$2500,sr			; Allow other MFP interrupts (ikbd, border) to run
 
-	sub.w	#50,tccount
+	sub.w	#50,tccount			; Allow variable play speed (here TC=50)
 	bpl.s	.skip
 	add.w	#200,tccount
 	movem.l	d0-a6,-(a7)
 	lea	player_state(pc),a0
 	lea	output_buffer,a6
-	eor.w	#$ff,$ffff8240.w
 	bsr	player_update
-	eor.w	#$ff,$ffff8240.w
+	move.w	#$777,$ffff8240.w
 	movem.l	(a7)+,d0-a6
-.skip
-	move.l	old_c,-(sp)
+.skip:	move.l	old_c,-(sp)
 	rts
 
-tccount	dc.w	200			;timer C down counter... this is a bit severe!
-old_c:	ds.l	1
+tccount			dc.w	200		;timer C down counter
+old_c:			ds.l	1
 
+
+; -----------------------------------------------------------------------
+;	CORE PLAYER CODE
 ; -----------------------------------------------------------------------
 NUM_REGS		equ	14
-cache_size		equ	512		; number of saved bytes per register
+cache_size		equ	512			; number of saved bytes per register
 
-						; KEEP THESE 3 IN ORDER
-ymunp_match_read_ptr	equ	0		; X when copying, the src pointer (either in cache or in original stream)
-ymunp_stream_read_ptr	equ	4		; position in packed data we are reading from
-ymunp_copy_count_w	equ	8		; number of bytes remaining to copy. Decremented at start of update.
-ymunp_size		equ	10		; structure size
+							; KEEP THESE 3 IN ORDER
+ymunp_match_read_ptr	equ	0			; X when copying, the src pointer (either in cache or in original stream)
+ymunp_stream_read_ptr	equ	4			; position in packed data we are reading from
+ymunp_copy_count_w	equ	8			; number of bytes remaining to copy. Decremented at start of update.
+ymunp_size		equ	10			; structure size
 
 ; -----------------------------------------------------------------------
+; a0 = start of packed ym data
 player_init:
 	; "globals" first
 	lea	cache(pc),a2
@@ -113,18 +109,20 @@ player_init:
 	; a3 = copy of packed file start
 	moveq.l	#NUM_REGS-1,d0
 .fill:
-	; a0 = input data (moves!)
+	; a0 = input data (this moves for each channel)
 	move.l	a3,d1
-	add.l	(a0)+,d1
-	move.l	d1,ymunp_stream_read_ptr(a1)		; ymunp_stream_read_ptr
-	clr.l	ymunp_match_read_ptr(a1)		; ymunp_match_read_ptr
-	move.w	#1,ymunp_copy_count_w(a1)		; ymunp_copy_count_w
+	add.l	(a0)+,d1				; read size offset in header
+	move.l	d1,ymunp_stream_read_ptr(a1)		; setup ymunp_stream_read_ptr
+	clr.l	ymunp_match_read_ptr(a1)		; setup ymunp_match_read_ptr
+	move.w	#1,ymunp_copy_count_w(a1)		; setup ymunp_copy_count_w
 	lea	ymunp_size(a1),a1			; next stream state
 	dbf	d0,.fill
 	rts
 
+; -----------------------------------------------------------------------
 ; a0 = input structure
 player_update:
+	move.w	#$700,$ffff8240.w
 	lea	player_state,a0				; a0 = streams state
 	lea	output_buffer,a6			; a6 = YM buffer
 	move.l	cache_write_ptr(pc),a2			; a2 = cache write ptr
@@ -181,30 +179,31 @@ literals:
 
 stream_copy_one:
 	; Copy byte from either the cache or the literals in the stream
-	move.l	ymunp_match_read_ptr(a0),a1	; a1 = match read
+	move.l	ymunp_match_read_ptr(a0),a1		; a1 = match read
 	; a2 = cache write, a3 = loop addr
-	move.b	(a1)+,d0			; d0 = output result
-	move.b	d0,(a2)				; add to cache. Don't need to increment
+	move.b	(a1)+,d0				; d0 = output result
+	move.b	d0,(a2)					; add to cache. Don't need to increment
 
 	; Handle the *read* pointer hitting the end of the cache
 	; The write pointer check is done in one single go since all sizes are the same
 	; This check is done even if literals are copied, it just won't ever pass the check
-	cmp.l	a3,a1				; has match read ptr hit end of cache?
+	cmp.l	a3,a1					; has match read ptr hit end of cache?
 	bne.s	noloop_cache_read
-	sub.w	d3,a1				; move back in cache
+	sub.w	d3,a1					; move back in cache
 noloop_cache_read:
 	move.l	a1,ymunp_match_read_ptr(a0)
 
 	; d0 is "output" here
-	move.b	d0,(a6)+			; write to output buffer
+	move.b	d0,(a6)+				; write to output buffer
 
-	add.w	d3,a2				; next cache_write_ptr
-	add.w	d3,a3				; next cache_end ptr
-	add.w	d2,a0				; next stream structure
-
+	; Move on to the next register
+	add.w	d3,a2					; next cache_write_ptr
+	add.w	d3,a3					; next cache_end ptr
+	add.w	d2,a0					; next stream structure
 	dbf	d1,stream_update
 	bra.s	ym_write
 
+; If the previous byte read was 0, read 2 bytes to generate a 16-bit value
 read_extended_number:
 	tst.b	d0
 	bne.s	valid_count
@@ -215,6 +214,8 @@ valid_count:
 	rts
 
 ym_write:
+	move.w	#$007,$ffff8240.w
+
 	; We could write these in reverse order and reuse a6?
 	lea	output_buffer(pc),a6
 	lea	$ffff8800.w,a0
@@ -227,10 +228,10 @@ r	set	0
 		move.b	(a6)+,(a1)
 	else
 		; Buzzer variant
-		move.b	(a6)+,d0
+		move.b	(a6)+,d0			; Buzzer envelope register is special case,
 		bmi.s	.skip_write
-		move.b	#r,(a0)
-		move.b	d0,(a1)
+		move.b	#r,(a0)				; only write if value is not -1
+		move.b	d0,(a1)				; since writing re-starts the envelope
 .skip_write:
 	endif
 r	set	r+1
@@ -246,14 +247,8 @@ r	set	r+1
 .no_cache_loop:
 	rts
 ; -----------------------------------------------------------------------
-
-
 		even
-;player_data:	incbin	led2.all.ymp
-;player_data:	incbin	sanxion.all.ymp
-
 ; -----------------------------------------------------------------------
-
 ; 
 tmp:		ds.w	1
 cache_write_ptr:ds.l	1
@@ -263,4 +258,7 @@ cache:		ds.b	cache_size*NUM_REGS
 output_buffer:	ds.b	NUM_REGS
 		even
 
-player_data:	incbin	motus.all.ymp
+player_data:
+	;	incbin	motus.all.ymp
+		incbin	sanxion.dat4
+		even
